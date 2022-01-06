f5db719bfb2fe891246b94c8557ce4701d71c42b
jungho <jungpark007@naver.com>
2022-01-06 19:14:36 +0900

    String 클래스에 대한 학습 테스트 - 요구사항 3

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -1,8 +1,11 @@
 package study;
 
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class StringTest {
     @Test
@@ -24,7 +27,7 @@ public class StringTest {
 
 
     @Test
-    void split2() throws Exception{
+    void split2() throws Exception {
         //    "1"을 ,로 split 했을 때 1만을 포함하는 배열이 반환되는지에 대한 학습 테스트를 구현한다.
         String oneAndTwo = "1";
 
@@ -34,11 +37,25 @@ public class StringTest {
     }
 
     @Test
-    void subString() throws Exception{
+    void subString() throws Exception {
         String oneAndTwo = "(1,2)";
 
-        String substring = oneAndTwo.substring(1, oneAndTwo.length()-1);
+        String substring = oneAndTwo.substring(1, oneAndTwo.length() - 1);
 
         assertThat(substring).isEqualTo("1,2");
     }
+
+    @Test
+    @DisplayName("String 의 주어진 범위를 벗아난 index 의 charAt 을 실행하면 StringIndexOutOfBoundsException 발생한다 ")
+    void charAt() throws Exception {
+
+        String abc = "abc";
+
+        assertThatThrownBy(() -> {
+            abc.charAt(3);
+        }).isInstanceOf(StringIndexOutOfBoundsException.class);
+
+    }
+
+
 }
