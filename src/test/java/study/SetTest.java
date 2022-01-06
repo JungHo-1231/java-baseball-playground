298466311b45e9971d5d22732eaa3f44433ee66a
jungho <jungpark007@naver.com>
2022-01-06 19:44:38 +0900

    String 클래스에 대한 학습 테스트 - SetTest 요구 사항 3

--- src/test/java/study/SetTest.java
+++ src/test/java/study/SetTest.java
@@ -4,6 +4,7 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.HashSet;
@@ -35,9 +36,14 @@ public class SetTest {
     @ValueSource(ints = {1, 2, 3})
     void checkContainNumber(int input) {
 
-        assertThat(number.contains(input)).isTrue();
-        
+        assertThat(this.number.contains(input)).isTrue();
+
     }
 
+    @ParameterizedTest
+    @CsvSource({"1,true", "2,true", "3,true", "4,false", "5,false"})
+    void checkContainNumberWithCsvSource(int input, boolean result) {
 
+        assertThat(number.contains(input)).isEqualTo(result);
+    }
 }
