78234268f91645914c062e05f0b22d4adf61e30f
jungho <jungpark007@naver.com>
2022-01-06 19:02:28 +0900

    String 클래스에 대한 학습 테스트 - 요구사항 1

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -10,4 +10,26 @@ public class StringTest {
         String actual = "abc".replace("b", "d");
         assertThat(actual).isEqualTo("adc");
     }
+
+    @Test
+    void split() throws Exception {
+        //    "1,2"을 ,로 split 했을 때 1과 2로 잘 분리되는지 확인하는 학습 테스트를 구현한다.
+
+        String oneAndTwo = "1,2";
+
+        String[] split = oneAndTwo.split(",");
+
+        assertThat(split).contains("1", "2");
+    }
+
+
+    @Test
+    void split2() throws Exception{
+        //    "1"을 ,로 split 했을 때 1만을 포함하는 배열이 반환되는지에 대한 학습 테스트를 구현한다.
+        String oneAndTwo = "1";
+
+        String[] split = oneAndTwo.split(",");
+
+        assertThat(split).contains("1");
+    }
 }
